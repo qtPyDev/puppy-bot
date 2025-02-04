9bc17c38236cac1e30998e1b23eb48cb71de9b54
qtPyDev <avataylor2023@gmail.com>
2025-02-04 07:42:20 +0000

    staging init

---
+++ bot_config.py
@@ -0,0 +1,17 @@
+# bot_config
+
+APP_NAME = 'BlueSky bot service'
+CMD_ESC = 'Ctrl+C'
+
+QUERY = 'puppy*'
+
+LEXICON = [
+    'so puppy',
+    'puppy',
+    'wehhhh puppy',
+    'puppy likes u',
+    'puppy thinks that being silly is the meaning of life !',
+    'haiiiii',
+    'haihaiiiii :3',
+    'puppy doesnt know many words wehhh'
+]
